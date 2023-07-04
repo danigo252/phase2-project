8e798b87b2499a7b948988a07f0177efb9885526
Daniel <danielgonzalez252@gmail.com>
2023-07-04 02:43:06 -0400

    Declare function Post

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -10,7 +10,7 @@ function MovieForm () {
             title: title
         };
 
-        const configObj = {
+        const configObj =  {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(newMovie)
